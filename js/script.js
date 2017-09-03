8fece16d059331ed35d39909000bb7e29dda5447
KeisiOo <jamesnsk90@yandex.ru>
2017-09-04 01:36:11 +0700

    Завершение, наверно

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var requestBtn = document.querySelector('.request-btn');
+var requestForm = document.querySelector('.request-form');
+
+requestBtn.addEventListener('click', function () {
+	requestForm.classList.remove('hidden');
+});
+
